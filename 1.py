523016919d66a6d8f2fed6d38d866bcecd58784d
Aravindhakumar U <144508903+U-Aravindhakumar@users.noreply.github.com>
2024-02-06 20:55:02 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,6 +1,6 @@
+#T20-worldcup-2022 analysis
 
 #data cleaning process
-
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
@@ -9,14 +9,12 @@ pio.templates.default = "plotly_white"
 data = pd.read_csv("t20 world cup 22.csv")
 print(data.head())
 
-#number of matches won by each team in Bar graph
+#number of matches won by each team in Bar graph...
 
 figure = px.bar(data, 
                 x=data["winner"],
-                title="Number of Matches Won by teams in t20 World Cup 2022")
+                title="Number of Matches Won by teams in t20-WorldCup 2022")
 figure.show()
-
-
 won_by = data["won by"].value_counts()
 label = won_by.index
 counts = won_by.values
@@ -26,8 +24,7 @@ colors = ['gold','lightgreen']
 
 fig = go.Figure(data=[go.Pie(labels=label, values=counts)])
 fig.update_layout(title_text='Number of Matches Won By Runs Or Wickets')
-fig.update_traces(hoverinfo='label+percent', textinfo='value', textfont_size=30,
-                  marker=dict(colors=colors, line=dict(color='black', width=3)))
+fig.update_traces(hoverinfo='label+percent', textinfo='value', textfont_size=30, marker=dict(colors=colors, line=dict(color='black', width=3)))
 fig.show()
 
 # Bat first or Bowled first
@@ -36,13 +33,12 @@ toss = data["toss decision"].value_counts()
 label = toss.index
 counts = toss.values
 colors = ['skyblue','yellow']
-
 fig = go.Figure(data=[go.Pie(labels=label, values=counts)])
 fig.update_layout(title_text='Toss Decisions in t20 World Cup 2022')
 fig.update_traces(hoverinfo='label+percent', textinfo='value', textfont_size=30, marker=dict(colors=colors, line=dict(color='black', width=3)))
 fig.show()
 
-#top scorerers 
+#top scorerers in T20 worldcup
 
 figure = px.bar(data, 
                 x=data["top scorer"], 
@@ -51,14 +47,14 @@ figure = px.bar(data,
                 title="Top Scorers in t20 World Cup 2022")
 figure.show()
 
-# no of player of matches
+# no.of player of the matche awards in particular players 
 
 figure = px.bar(data, 
                 x = data["player of the match"], 
                 title="Player of the Match Awards in t20 World Cup 2022")
 figure.show()
 
-# best bowling figers
+# best bowlers in each match 
 
 figure = px.bar(data, 
                 x=data["best bowler"],
@@ -85,7 +81,7 @@ fig.update_layout(barmode='group',
                   title="Best Stadiums to Bat First or Chase")
 fig.show()
 
-#1st and 2nd bowling in every wicket
+# best stetium in bowl 1st or 2nd
 
 fig = go.Figure()
 fig.add_trace(go.Bar(
